a17c8dbc660199b560f891cc9d5ed4d8ebcd3e3d
CHEN Jiajie <gameboyvito@gmail.com>
2017-06-10 11:33:04 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+
+import { NativeModules } from 'react-native';
+
+const { RNAliyunOss } = NativeModules;
+
+export default RNAliyunOss;
---
+++ ios/RNAliyunOss.m
@@ -0,0 +1,13 @@
+
+#import "RNAliyunOss.h"
+
+@implementation RNAliyunOss
+
+- (dispatch_queue_t)methodQueue
+{
+    return dispatch_get_main_queue();
+}
+RCT_EXPORT_MODULE()
+
+@end
+  
